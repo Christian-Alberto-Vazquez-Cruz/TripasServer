b11a4653cb9abb36cf761e67e386855089cefe08
Christian Alberto Vázquez Cruz <zS22013636@estudiantes.uv.mx>
2024-10-17 00:55:26 -0600

    Contrato ChatManager, Servicio por definir

---
+++ TripasService/Services/ChatManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripasService.Contracts;
+
+namespace TripasService.Services {
+    public partial class TripasGameService : IChatManager {
+        public void connectToLobby(int roomId, string username) {
+            throw new NotImplementedException();
+        }
+
+        public void leaveLobby(string roomId, string username) {
+            throw new NotImplementedException();
+        }
+
+        public void sendMessage(Message message) {
+            throw new NotImplementedException();
+        }
+    }
+}
