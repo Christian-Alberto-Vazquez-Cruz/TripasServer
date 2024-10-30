1586a59ef5f0de352a90025dc346f2f188044142
Christian Alberto Vázquez Cruz <zS22013636@estudiantes.uv.mx>
2024-10-30 10:30:33 -0600

    Cambios en el método de Login

---
+++ TripasService/Enums/GameEnums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripasService.Enums {
+    public static class GaneEnums {
+        public enum PlayerStatus {
+            Offline,
+            Online,
+            InGame
+        }
+
+        public enum LobbyStatus {
+            Open,
+            Closed,
+            InProgress
+        }
+    }
+}
