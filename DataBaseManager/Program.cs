4b653c8ee579ff64c739a0bdab8723126d048719
Erick Abdiel Atzin Olarte <162238261+ERICKABDIEL@users.noreply.github.com>
2024-12-06 00:11:27 -0600

    cambios depsues de las pruebas

--- DataBaseManager/Program.cs
+++ DataBaseManager/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataBaseManager {
-    public class Program {
+    internal class Program {
         static void Main(string[] args) {
-
         }
     }
 }
